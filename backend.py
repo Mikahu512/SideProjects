ea44aabe4ffc9bb87d232f9241f5aba78e5c7cdd
Mika <77134715+Mikahu512@users.noreply.github.com>
2021-05-08 16:35:08 -0500

    Update backend.py

--- backend.py
+++ backend.py
@@ -2,11 +2,13 @@
 #backend.py
 import sqlite3
 class Database:
+    
     def __init__(self, db):
         self.conn=sqlite3.connect(db)
         self.cur=self.conn.cursor()
         self.cur.execute("CREATE TABLE IF NOT EXISTS book (id INTEGER PRIMARY KEY, title text, author text, year integer, isbn integer)")
         self.conn.commit()
+        
     def insert(self,title,author,year,isbn):
         self.cur.execute("INSERT INTO book VALUES (NULL,?,?,?,?)",(title,author,year,isbn))
         self.conn.commit()
