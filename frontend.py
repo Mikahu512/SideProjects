48300e961870ef5ccb551460cbd28cef9aa01f16
Mika <77134715+Mikahu512@users.noreply.github.com>
2021-09-18 22:18:24 -0500

    Update frontend.py

--- frontend.py
+++ frontend.py
@@ -28,7 +28,6 @@ class Window(object):
         self.author_text=StringVar()
         self.e2=Entry(window,textvariable=self.author_text)
         self.e2.grid(row=0,column=3)
-
         
         self.year_text=StringVar()
         self.e3=Entry(window,textvariable=self.year_text)
